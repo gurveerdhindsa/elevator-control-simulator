ab2d1b56ec716c806fb4b913af6f4593299b03d8
Gurveer Dhindsa <gurveerdhindsa@hotmail.com>
2019-01-23 12:15:09 -0500

    Updated Floor class

--- src/main/Floor.java
+++ src/main/Floor.java
@@ -14,25 +14,15 @@ public class Floor {
 	   					   receivePacket;		// Packet to receive from Scheduler
 	private DatagramSocket sendReceiveSocket;	// Socket to send and receive packets
 	
-	// For now, we'll use a HashMap to store the configuration & assume one set of input
-	// Data Types: Time: Timestamp, Floor: Integer, Floor Button: String, Car Button: Integer
-	private HashMap<String, Object> config;
-	
 	// Constants
 	private static final int DESTINATION_PORT = 23;
 	
 	public Floor() {
-		config = new HashMap<String,Object>();
+		
+		System.out.println("Importing configuration...");
 		
 		// Import from configuration.txt
 		importConfiguration();
-		
-		
-		// *Optional* Log the configuration we've imported
-		System.out.println("Imported configuration: ");
-		for (Map.Entry<String, Object> entry : config.entrySet()) {
-			System.out.println("\t" + entry.getKey() + " : " + entry.getValue());
-		}
 		
 		try {
 			// Create datagram socket and bind to any available port
@@ -48,24 +38,31 @@ public class Floor {
 	public void importConfiguration() {
 		try (BufferedReader br = new BufferedReader(new FileReader("src/configuration/configuration.txt"))) {
 			String input = null;
+			
+			Timestamp timestamp = null;
+			int floor = 0, carButton = 0;
+			String floorButton = null;
+			
 			while ((input = br.readLine()) != null) {
 				String [] inputFields = input.split(":",2);				
 				if (inputFields[0].equals("Time")) {
 					try {
 					    SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss.SSS");
 					    Date parsedDate = dateFormat.parse(inputFields[1]);
-					    Timestamp timestamp = new Timestamp(parsedDate.getTime());
-
-						config.put(inputFields[0], timestamp);
+					    timestamp = new Timestamp(parsedDate.getTime());
 					} catch (ParseException e) {
 						e.printStackTrace();
 					}
-				} else if (inputFields[0].equals("Floor") || inputFields[0].equals("Car Button")) {
-					config.put(inputFields[0], Integer.parseInt(inputFields[1].trim()));
+				} else if (inputFields[0].equals("Floor")) {
+					floor = Integer.parseInt(inputFields[1].trim());
+				} else if (inputFields[0].equals("Car Button")) {
+					carButton = Integer.parseInt(inputFields[1].trim());
 				} else if (inputFields[0].equals("Floor Button")){
-					config.put(inputFields[0], inputFields[1].trim());
+					floorButton = inputFields[1].trim();
 				}
 			}
+			
+			FloorRequest floorRequest = new FloorRequest(timestamp, floor, carButton, floorButton);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
