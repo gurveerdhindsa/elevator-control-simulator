fa7492edf604c843809caaea9019b9b4adbc12e2
rohank07 <rohank_17@hotmail.ca>
2019-01-27 13:53:20 -0500

    Read in floorRequest

--- src/test/FloorTest.java
+++ src/test/FloorTest.java
@@ -10,60 +10,100 @@ import java.net.InetAddress;
 
 import org.junit.Test;
 
+import main.Elevator;
 import main.Floor;
 public class FloorTest {
 
 
-
-	@Test
-	public void testReceiveRequest()
+	
+	
+	@SuppressWarnings("deprecation")
+	@Test 
+	// start listening to the Floor thread
+	public void testRegistration()
 	{
-		
 		Thread floorThread = new Thread(new Runnable()
 		{
-		@Override
-		public void run()
-		{
-			Floor f = new Floor();
-			f.run();
-		}
+			@Override
+			public void run()
+			{
+				Floor floor = new Floor();
+			}
+		});
 		
-	});
+		byte[] regist = new byte[50];
 		
-		floorThread.start();
-		byte[] request = new byte[] {0};
 		try {
-			DatagramPacket requestPckt = new DatagramPacket(request,request.length,
-					InetAddress.getLocalHost(),45);
-			DatagramSocket sendReq = new DatagramSocket();
-			sendReq.send(requestPckt);
-			sendReq.close();
+			DatagramPacket registration = new DatagramPacket(regist,regist.length);
+			DatagramSocket getRegist = new DatagramSocket(45);
+			floorThread.start();
+			getRegist.receive(registration);
+			// read in buffer 
+			
+			
+			
+			
+			getRegist.close();
 		} catch (IOException e) {
-		
-			fail("Did not send ");
-			e.printStackTrace();
-		} 
-		
-		byte[] arrival = new byte[100];
-		try {
-			Thread.sleep(19500);
-			DatagramSocket getArrival = new DatagramSocket(69);
-			
-			DatagramPacket pck = new DatagramPacket(arrival,arrival.length);
-			getArrival.receive(pck);
-			getArrival.close();
-			
-			
-		} catch (InterruptedException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		assertTrue((int)arrival[0] == 4);
-		assertTrue((int)arrival[1] == 0);
+		assertTrue((int)regist[0] == 0);
+		assertTrue((int)regist[4] == 33);
 		
-		
+		floorThread.stop();
 	}
+//	@Test
+//	public void testReceiveRequest()
+//	{
+//		
+//		Thread floorThread = new Thread(new Runnable()
+//		{
+//		@Override
+//		public void run()
+//		{
+//			Floor f = new Floor();
+//			f.start();
+//		}
+//		
+//	});
+//
+//		byte[] request = new byte[] {0};  
+//		try {
+//			DatagramPacket requestPckt = new DatagramPacket(request,request.length,
+//					InetAddress.getLocalHost(),45);
+//			DatagramSocket sendReq = new DatagramSocket();
+//			floorThread.start();
+//			sendReq.send(requestPckt);
+//			sendReq.close();
+//		} catch (IOException e) {
+//		
+//			fail("Did not send ");
+//			e.printStackTrace();
+//		} 
+//		
+//		byte[] arrival = new byte[100];
+//		try {
+//			Thread.sleep(19500);
+//			DatagramSocket getArrival = new DatagramSocket(69);
+//			
+//			DatagramPacket pck = new DatagramPacket(arrival,arrival.length);
+//			getArrival.receive(pck);
+//			getArrival.close();
+//			
+//			
+//		} catch (InterruptedException | IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		assertTrue((int)arrival[0] == 4);
+//		assertTrue((int)arrival[1] == 0);
+//		
+//		
+//	}
+//	
 		
 		
 	
